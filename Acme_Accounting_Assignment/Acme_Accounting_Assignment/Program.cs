c8221b342a213b10febe677d19c541ea14219dce
Leslie <106999007+LuvlyLess@users.noreply.github.com>
2022-10-07 11:22:28 -0700

    Acme Accounting CSharp Basic Assignment
    Basic C# Submission Assignment, build a welcome screen for new accounting software

---
+++ Acme_Accounting_Assignment/Acme_Accounting_Assignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace CSharp_Assignments
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string Welcome = "Hello ";
+            string Acme = "Welcome to Acme Accounting Systems\n \nRemember, we’re “accounting” on you! \n";
+            string accountNumber = "What's your Account Number";
+            string AcmeAccNum = Welcome + accountNumber;
+            Console.WriteLine(AcmeAccNum);
+            string accNum = Console.ReadLine();
+            Console.WriteLine(Acme + accNum);
+            Console.ReadLine();
+        }
+    }
+}
+
