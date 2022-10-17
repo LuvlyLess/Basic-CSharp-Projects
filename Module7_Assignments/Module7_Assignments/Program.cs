db428436b0e6b3076e10544a8bddb368ba42c420
Leslie <106999007+LuvlyLess@users.noreply.github.com>
2022-10-17 11:17:45 -0700

    Math Operation Assignment Submission
    Math Operation Assignment Submissions For Module 7 C# Basic Project Submission

---
+++ Module7_Assignments/Module7_Assignments/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module7_Assignments_Math_AND_Compairson_Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            //                                       input from the user, multiplies it by 50, and prints the result
+
+            Console.WriteLine("Please Pick a Number");
+            int userNumber = int.Parse(Console.ReadLine());            
+            Console.WriteLine("Your Number Times fifty is " + userNumber * 50);
+            Console.ReadLine();
+
+
+
+            //                                      input from the user, adds 25 to it, and prints the result  
+
+            Console.WriteLine("Please Pick a Number");
+            int userNumberADD = int.Parse(Console.ReadLine());
+            Console.WriteLine("Your Number Plus TwentyFive is " + userNumberADD + 25);
+            Console.ReadLine();
+
+
+            //                                      input from the user, divides it by 12.5, and prints the result
+
+
+
+            Console.WriteLine("Please Pick a Number");
+            double userNumberDIVIDE = int.Parse(Console.ReadLine());
+            Console.WriteLine("Your Number Divided by 12.5 is " + userNumberDIVIDE / 12.5);
+            Console.ReadLine();
+
+
+            //                                        Input from the user, checks if it is greater than 50, and prints the true/false
+
+            Console.WriteLine("Please Pick a Number");
+            int userNumberGreater = int.Parse(Console.ReadLine());
+            Console.WriteLine("Is Your Number Greater Than 50 ");
+            Console.WriteLine(userNumberGreater > 50);
+            Console.ReadLine();
+
+            //                                      input from the user, divides it by 7, and prints the remainder
+
+            Console.WriteLine("Please Pick a Number");
+            double userNumberDIVIDE = int.Parse(Console.ReadLine());
+            Console.WriteLine("Your Number Divided by 7 is " + userNumberDIVIDE % 7);
+            Console.ReadLine();
+
+
+        }
+    }
+}
