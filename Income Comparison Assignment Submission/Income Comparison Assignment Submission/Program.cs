3c33a15bc1064715d6fd973e1f3c3024b47b9353
Leslie <106999007+LuvlyLess@users.noreply.github.com>
2022-10-17 11:49:29 -0700

    Income Comparison Assignment Submission
    Requirements
    
    The program must start by printing “Anonymous Income Comparison Program” to the screen.
    It must then print “Person 1” to the screen and get the following details:
    Hourly Rate
    Hours worked per week
    It must then print “Person 2” to the screen and then get the following details:
    Hourly rate
    Hours worked per week
    It must then print to the screen “Annual salary of Person 1:” and write the exact salary below it.
    It must then print to the screen “Annual salary of Person 2:” and write the exact salary below it.
    It must then print to the screen “Person 1 makes more money than Person 2” and write the true or false value of this statement below it.

---
+++ Income Comparison Assignment Submission/Income Comparison Assignment Submission/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Income_Comparison_Assignment_Submission
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Anonymous Income Comparison Program \n");
+
+            string Person1 = "John Smith \n";
+
+            int Person1Wage = 15 ;
+            
+            int Person1Hours = 40 ;
+            
+            string Person2 = "Jane Doe \n";
+
+            int Person2Wage = 20 ;
+
+            int Person2Hours = 40 ;
+
+            int Person1Salary = 31200 ;
+
+            int Person2Salary = 41600 ;
+
+
+            Console.WriteLine(Person1 + " " + Person1Hours + " " + Person1Wage + " " + Person1Salary);
+            Console.WriteLine(Person2 + " " +  Person2Hours + " " + Person2Wage + " " + Person2Salary);
+
+            Console.WriteLine(Person1Salary > Person2Hours);
+
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
