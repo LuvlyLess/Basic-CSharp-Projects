651dcb76f9098d9ab2f263c85375585833ab23c2
Leslie <106999007+LuvlyLess@users.noreply.github.com>
2022-10-18 15:31:57 -0700

    Boolean Logic Assignment
    equirements
    
    Ask the following questions:
    
    What is your age?
    Have you ever had a DUI?
    How many speeding tickets do you have?
    Use the following qualification rules to determine if the applicant qualifies for car insurance:
    
    Applicants must be over 15 years old.
    Applicants must not have any DUIs.
    Applicants must not have more than 3 speeding tickets.
    Print the result of the Boolean expression created from the above business rules

---
+++ Boolean_Logic_Assignment_Submission/Boolean_Logic_Assignment_Submission/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean_Logic_Assignment_Submission
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //                               Age statement 
+            Console.WriteLine("Welcome to the Approval program \n please answer honestly \n Press Enter ");
+            Console.WriteLine("What is your age \n ");
+            int clientAge = Convert.ToInt32(Console.ReadLine());
+
+            //                               DUI statement
+            bool DUIStatus;
+            Console.Write("Have You ever been charged with a DUI (True || False answer only)");
+            DUIStatus = bool.Parse(Console.ReadLine());
+
+
+            //                            Speeding ticket statment 
+            Console.WriteLine("How many speeding tickets do you have? \n");
+            string v = Console.ReadLine();
+            int speedingTicket = Convert.ToInt16(v);
+
+            
+            if (clientAge >= 15 && DUIStatus == false && speedingTicket < 3)
+            {
+                Console.WriteLine(clientAge + " " + DUIStatus + " " + speedingTicket + " Your Approved ");
+            }
+            else
+            {
+                Console.WriteLine(" You're did not get Approved");
+            }
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }  
+    }
+}
