da1115539235366f427cfefc4acc701011ae7885
Leslie <106999007+LuvlyLess@users.noreply.github.com>
2022-11-09 10:23:44 -0800

    Console App assignment Strings and Int submission
    create a console app that includes the following
    Create a list of integers. Ask the user for a number to divide each number in the list by. Write a loop that takes each integer in the list, divides it by the number the user entered, and displays the result to the screen.

---
+++ Console App Strings and Integers Assignment/Console App Strings and Integers Assignment/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Console_App_Strings_and_Integers_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // try catch statement with exception catching 
+
+            try
+            {
+                List<int> numbers = new List<int>() { 10, 15, 20 }; // My  list of integers
+
+                Console.WriteLine("Please Pick a Number ");
+                int UserInput = Convert.ToInt32(Console.ReadLine()); // User Number input parsed to convert to int
+
+                
+
+
+                for (int i = 0; i < numbers.Count; i++) // for loop 
+                {
+                    Console.WriteLine("My array List index numbers Divided by Your Number is \n" + numbers[i] / UserInput);
+                }
+
+            }
+            catch (FormatException ex) // My catch format errors statements
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            catch (DivideByZeroException ex) 
+            {
+                Console.WriteLine("please don't divide by zero ");
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("End of try catch statement"); //program print a message to the display to let you know the program has emerged from the try/catch block and continued on with program
+                Console.ReadLine();
+            }
+
+
+
+
+
+
+
+
+
+
+
+        }
+        }
+}
