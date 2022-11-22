d880fb28068c4ffb66639d7ff3147285ace4c3b5
Leslie <106999007+LuvlyLess@users.noreply.github.com>
2022-11-22 11:41:18 -0800

    Struct Assignment
    console app that includes the following:
    
    Create a struct called Number and give it the property “Amount” and have it be of data type decimal.
    
    In the Main() method, create an object of data type Number and assign an amount to it.
    
    Print this amount to the console.

---
+++ Struct-Assignment/Struct-Assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Assignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // structs
+            Numbers numbers = new Numbers();
+            Numbers numbers1 = numbers;
+            numbers1.Amount = (decimal)12.99;
+            
+            Console.WriteLine(" My struck decimal Amount " + numbers1.Amount);
+            Console.ReadLine();
+           // var number2 = (Numbers)Enum.Parse(typeof(Numbers), numbers.ToString());
+
+        }
+        
+    }
+}
