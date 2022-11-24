8d320006d572faf3249272184a88b5edf402f0a0
Leslie <106999007+LuvlyLess@users.noreply.github.com>
2022-11-24 10:55:35 -0800

    DateTime Assignment
    create a console app that includes the following:
    
    Prints the current date and time to the console.
    
    Asks the user for a number.
    
    Prints to the console the exact time it will be in X hours, X being the number the user entered in step 2.
    
    Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.

---
+++ DateTime-Assignment/DateTime-Assignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DateTime_Assignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The current DateTime is \n"); 
+            Console.WriteLine(DateTime.Now);// Writing the current datetime to the console using DateTime.Now
+            
+
+            Console.WriteLine("Please enter a Number...\n"); // asking for user input
+            int UserInput = Convert.ToInt32(Console.ReadLine()); // converting to int
+
+
+
+            DateTime dateTime = DateTime.Now; // creating a instance of current datetime
+           
+            Console.WriteLine(dateTime.AddHours(UserInput)); // adding user input in hours to the current date time
+            
+            Console.ReadLine();
+        }
+    }
+}
