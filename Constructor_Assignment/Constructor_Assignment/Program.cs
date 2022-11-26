305b1081204563bacd668f56c7c4ea5c151a91f2
Leslie <106999007+LuvlyLess@users.noreply.github.com>
2022-11-26 10:06:18 -0800

    Constructor Assignment
     console app that includes the following:
    
    Create a const variable.
    
    Create a variable using the keyword “var.”
    
    Chain two constructors together.

---
+++ Constructor_Assignment/Constructor_Assignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const string ConsoleName = "Console: HelloWorld"; // creating a const for a greeting
+            Console.WriteLine("Welcome to {0}. Whats Your Name ?", ConsoleName); // printing the const greeting and asking name
+            var UserName = Console.ReadLine(); // creating a variable using var
+
+
+
+
+            // instantiating constructor
+            var objOne = new SampleClass(10); // calling objs
+            var obj2 = new SampleClass(firstname:"Tim");
+            var obj3 = new SampleClass(10, firstname: "Monkey");
+
+            
+        }
+    }
+}
---
+++ Constructor_Assignment/Constructor_Assignment/SampleClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Assignment
+{
+    public class SampleClass
+    {
+        private int ID;
+        private string firstname;
+        
+
+        public SampleClass(string firstname) : this(100, "") //defining multiple constructors to chain
+        {// assigning firstname to firstname , providing a default ID if none input
+            
+        }
+
+
+        public SampleClass(int ID, string firstname)
+        {
+            this.ID = ID;
+            this.firstname = firstname;
+        }
+
+
+        public SampleClass(int ID) : this(ID, "")
+        {
+           
+        }
+
+       
+        
+          
+    }
+}
