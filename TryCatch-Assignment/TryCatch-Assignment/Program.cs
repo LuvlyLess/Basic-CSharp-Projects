f3df190284e44f38aed2fdad4e1ebf9c95c8c7a0
Leslie <106999007+LuvlyLess@users.noreply.github.com>
2022-11-26 11:24:15 -0800

    Try/Catch Assignment
    create a console app that includes the following:
    
    Ask the user for their age.
    
    Display the year the user was born.
    
    Exceptions must be handled using “try/catch.”
    
    Display appropriate error messages if the user enters zero or negative numbers.
    
    Display a general message if an exception was caused by anything else.

---
+++ TryCatch-Assignment/TryCatch-Assignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try // using a try catch to catch exceptions
+            {
+                Console.WriteLine("Please enter you Age");
+                int UserAge = Convert.ToInt32(Console.ReadLine());  // converting userinput
+                var currentDate = DateTime.Now; // getting the current year to compare
+                Console.WriteLine(UserAge - DateTime.Now.Year + " The year you were born"); // printing the year they were born
+                
+                if (UserAge < 0) // using
+                {
+                    throw new Exception("Negative Age is Not Possible"); // creating a simple exception message for user
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please use correct Format, Digits Only"); 
+            }
+            Console.ReadLine();
+        }
+    }
+}
